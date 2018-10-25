5757e1b93e4c8831ea6c59da07b1d21a6bde9752
Caio Oliveira <caioaao@gmail.com>
2018-10-25 14:38:20 -0300

    circleci stuff

--- project.clj
+++ project.clj
@@ -6,4 +6,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha3"]
-                                  [nubank/matcher-combinators "0.4.2"]]}})
+                                  [nubank/matcher-combinators "0.4.2"]]
+                   :plugins      [[lein-cljfmt "0.6.1"]]}}
+  :release-tasks [["deploy" "clojars"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "patch"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :sign-releases false
+                                    :creds         {:username :env/clojars_username
+                                                    :password :env/clojars_password}}]])
